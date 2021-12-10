6e30ab8abd7289e5c524bc701a466f85fe3275b8
ajayparihar876 <ajayparihar876@gmail.com>
2021-12-10 19:03:01 +0530

    sql+forloops

---
+++ sql.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+dbname = input(" Enter the database NAME  ")
+tablename = input("Enter the table name  ")
+store = input("how many student you want to store  ")
+# All value stored in var is string😁
+#we want int datatype in store var
+stored = int(store)                                              
+database = dbname+'.sqlite3'
+# know we open a file🤑
+fo = open(database,"a")
+
+con = sqlite3.connect(database)
+cur = con.cursor()
+#table ccreating 😎
+cur.execute(f"create table {tablename}(name text, email text, mobile text, address)")
+for x in range(stored):
+    n = input("Enter the student name  ")
+    m = input("Enter the email id   ")
+    o = input("Enter the student mobile number  ")
+    p = input("Enter the student Address  ")
+
+    cur.execute(f"Insert into {tablename} VALUES ('{n}','{m}','{o}','{p}')")    
+con.commit()
+con.close()
+fo.close()
+
